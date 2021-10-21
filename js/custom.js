b08b8b3bbfbbb594c2550410658dda7867b3d3d1
caimm <1569606597@qq.com>
2021-10-21 12:02:36 +0800

    页面优化

--- js/custom.js
+++ js/custom.js
@@ -8,11 +8,11 @@ $(window).load(function(){
 
 // HOME BACKGROUND SLIDESHOW
 $(function(){
-    /*jQuery(document).ready(function() {
+    jQuery(document).ready(function() {
 		$('body').backstretch([
 	 		 "images/tm-bg-slide-1.jpg", 
 	 		 "images/tm-bg-slide-2.jpg",
 			 "images/tm-bg-slide-3.jpg"
 	 			], 	{duration: 3200, fade: 1300});
-		});*/
+		});
 })
